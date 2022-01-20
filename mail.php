d1c8033c086338bcbfa413f80433c37dc576e01a
tulikamitra123 <97872860+tulikamitra123@users.noreply.github.com>
2022-01-20 19:53:09 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailheader = "From: ".$name."<".$email.">\r\n"
+$recipient = "tulika.mitra130702@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader)
+or die("Error! sorry try again next time :)");
+echo "gotcha! Thank you for you message";
+ 
+
+?>
